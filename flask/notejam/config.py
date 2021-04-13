893effe6c558c07df1e5afb347176bd5136edcd2
kiryllbondar <32882761+kiryllbondar@users.noreply.github.com>
2021-04-13 22:15:58 +0200

    Update config.py

--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -4,8 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
-    WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'mysql://notejamuser@notejam-mysql:FDFDd7837dsF@notejam-mysql.mysql.database.azure.com:3306/notejamdb' + os.path.join(basedir, 'notejam.db')
+    SECRET_KEY = 'notejam-flask-secret-key'
+    WTF_CSRF_ENABLED = True
+    CSRF_SESSION_KEY = 'notejam-flask-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'mysql://notejamuser@notejam-mysql:FDFDd7837dsF@notejam-mysql.mysql.database.azure.com:3306/notejamdb'
 
 
 class ProductionConfig(Config):
