fe26a6c2a7d5d9c3af6a0cd40626900277b7b963
kiryllbondar <32882761+kiryllbondar@users.noreply.github.com>
2021-04-13 22:43:26 +0200

    Create schema.sql

---
+++ flask/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS notejamdb.user(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(75) NOT NULL,
+    password VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS notejamdb.pad (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE notejamdb.note (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    pad_id INTEGER REFERENCES pads(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    name VARCHAR(100) NOT NULL,
+    text text NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+);
